556f558364e8fbd92baf20c8075af550ef71f19a
raphaellecorneau <r_corneau@hotmail.fr>
2017-01-20 11:45:16 +0100

    Update deploy.sh
    Ajout de commandes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
+#command on the server
 
+apt-get update
+apt-get upgrade
+apt-get install nginx
+apt-get install git
+
+cd /var/www/html/webcloud
+git clone 
+# crée un répertoire /var/www/html/webcloud/
+
+#do someother things
+
+cp index.html /var/www/html
