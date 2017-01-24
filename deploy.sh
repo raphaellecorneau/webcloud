368d9852ac052c325e73ffac1045fb2b70e3f144
Marion Gérin-roze <marion.gerin-roze@isep.fr>
2017-01-24 23:00:01 +0100

    Update deploy.sh
    finir le fichier deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,24 @@
 
 
-#command on the server
-
-apt-get update
+#commandes sur le serveur
+apt-get update 
 apt-get upgrade
-apt-get install nginx
-apt-get install git
+apt-get install nginx #installation du serveur web
+apt-get install git #installation de git
 
+#on se place dans le dossier 
+cd /var/www/html
+
+#on récupère le contenu du dépot git
+git clone https://github.com/raphaellecorneau/webcloud.git
+
+#on créé un répertoire /var/www/html/webcloud/
 cd /var/www/html/webcloud
-git clone 
-# crée un répertoire /var/www/html/webcloud/
 
 #on copie le fichier dans webcloud
 cp index.html /var/www/html/webcloud
+
+
+
+
+
